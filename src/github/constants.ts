d9a8095130923b38f88f0e1596358748c28c9929
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-07-04 17:56:31 +0300

    Add single pull request page merge listener

---
+++ src/github/constants.ts
@@ -0,0 +1,5 @@
+export const MERGE_QUERY_PARAM = 'MERGE_DEPENDABOT_PR';
+
+export default {
+  MERGE_QUERY_PARAM,
+};
--- src/github/selectors.ts
+++ src/github/selectors.ts
@@ -5,6 +5,10 @@ export const SELECTORS = {
   GITHUB_ISSUE_ELEMENT: '.js-issue-row',
   GITHUB_ISSUE_OPENED_BY: '.opened-by a',
   GITHUB_ISSUE_LINK: '.js-navigation-open',
+  GITHUB_NEW_COMMENT_FORM: '.js-new-comment-form',
+  GITHUB_NEW_COMMENT_FIELD: '#new_comment_field',
+  GITHUB_PR_AUTHOR: 'a.author',
+  GITHUB_COMMENT_BUTTON_TEXT: 'Comment',
 };
 
 export default {
