9d8e6a01c7d21c7b0ef2450139cc90c2fbcd426d
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-07-05 11:31:21 +0300

    Open PR tabs sequentially
    Github client side codes seems to need tab to be "active" to properly kick off the processes. Instead of openinig them all at once, open them sequentially and give it couple of seconds to process. Seems to work.
    
    Obviously much slower than adding all comments at once. May need to tweak how fast it should be.
    
    Closes #23

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,6 +2,10 @@ export const getRandomWaitBetween = (min: number, max: number): number => (
   Math.floor(Math.random() * (max - min) + min)
 );
 
+// eslint-disable-next-line no-promise-executor-return
+export const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export default {
   getRandomWaitBetween,
+  wait,
 };
